5565cc55a0fb0f6f92e3020b0df36b90096da36c
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-03-13 16:19:07 +0000

    ch

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const API_KEY = `49323961-66159e96cf1899f53bae32983`;
+const BASE_URL = `https://pixabay.com/api/`;
+
+export async function fetchImages(query) {
+   try{
+    const response = await axios.get(BASE_URL,{
+        params:{
+            key: API_KEY,
+            q: query,
+            image_type: `photo`,
+            orientation: `horizontal`,
+            satisfies: true,
+        }
+    });
+
+
+    console.log(response.data);
+    
+    return response.data.hits;
+
+   } catch(error){
+console.error(`Error fetching images:`, error)
+throw error
+   }
+   
+}
